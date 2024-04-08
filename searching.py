d5df82a5622522ced212b6c4f7649ca89c25020e
Adam Brez <breziinaadam@seznam.cz>
2024-04-08 09:03:15 +0200

    zkouska

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -12,10 +13,24 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    #Načtení povolených klíčů ze souboru
+    with open("sequential.json", "r") as f:
+        allowed_key = json.load(f)
 
+    #Ověření, zda je zadaný klíč v množině povolených klíčů
+    if field not in allowed_key:
+        return None
+
+    with open(file_name, "r") as f:
+        data = json.load(f)
+    # Vrácení hodnot
+    return data.get(field)
 
 def main():
-    pass
+    #Zavolání funkce read_data s požadovanými vstupy
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    #vytisknuti obsahu
+    print(sequential_data)
 
 
 if __name__ == '__main__':
